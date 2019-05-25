ff701f484887240af52a0d583eaa0346870b68d6
p_bavaz <paul.bavazzano@gmail.com>
2019-05-25 18:23:47 +0200

    fix imports and tests

---
+++ choco/core/model.py
@@ -0,0 +1,25 @@
+from jnius import autoclass
+
+from . import intvar
+
+class Model(object):
+
+
+    def getName(self):
+        return self.model.getName()
+
+    def intVar(self, name, lb, ub):
+        return self.model.intVar(name, lb, ub)
+
+    def arithm(self, var1, op1, var2, op2, cste):
+        self.model.arithm(var1, op1, var2, op2, cste).post()
+
+    def times(self, var1, var2, equ):
+        self.model.times(var1, var2, equ).post()
+
+    def solve(self):
+        self.model.getSolver().solve()
+
+    def __init__(self, name):
+        Mod = autoclass('org.chocosolver.solver.Model')
+        self.model = Mod(name)
