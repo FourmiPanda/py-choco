95a283e3b1ff9a0f7e2e93ad596c945d126e596a
tigerIonic <ceuterickx.gaetan@gmail.com>
2019-06-21 12:57:02 +0200

    demo.test2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ HERE = pathlib.Path(__file__).parent
 # This call to setup() does all the work
 setup(
     name="PyChoco",
-    version="1.0.11",
+    version="1.0.12",
     description="A python adapter for choco-solver",
     long_description="README",
     long_description_content_type="text/markdown",
